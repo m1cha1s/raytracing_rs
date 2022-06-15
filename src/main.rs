e371df96dbd40fc7ce37969de58f8d0df24e4bd6
m1cha1s <msulej04@gmail.com>
2022-06-15 15:24:57 +0200

    Basics done. Next creating a sphere

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,68 @@
+use raylib::prelude::*;
+use vec3::Vec3;
+
+mod ray;
+mod vec3;
+
+fn ray_color(ray: ray::Ray) -> Vec3 {
+    let unit_direction = Vec3::normalize(ray.dir);
+    let t = 0.5 * (unit_direction.y + 1.0);
+    (Vec3::new(1., 1., 1.)) * (1.0 - t) + Vec3::new(0.5, 0.7, 1.0) * t
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Image
+
+    let aspect_ratio: f64 = 16. / 9.;
+    let width: i32 = 400;
+    let height: i32 = (width as f64 / aspect_ratio) as i32;
+
+    // Camera
+
+    let viewport_height: f64 = 2.;
+    let viewport_width: f64 = aspect_ratio * viewport_height;
+    let focal_lenght: f64 = 1.;
+
+    let origin: Vec3 = Vec3::new(0., 0., 0.);
+    let horizontal: Vec3 = Vec3::new(viewport_width, 0., 0.);
+    let vertical: Vec3 = Vec3::new(0., viewport_height, 0.);
+    let lower_left_corner: Vec3 =
+        origin - horizontal / 2. - vertical / 2. - Vec3::new(0., 0., focal_lenght);
+
+    let (mut rl, thread) = raylib::init()
+        .size(width, height)
+        .title("Raytracing")
+        .build();
+
+    let mut d = rl.begin_drawing(&thread);
+
+    d.clear_background(Color::BLACK);
+
+    for j in 0..height {
+        for i in 0..width {
+            let u = i as f64 / (width - 1) as f64;
+            let v = j as f64 / (height - 1) as f64;
+            let r: ray::Ray = ray::Ray::new(
+                origin,
+                lower_left_corner + horizontal * u + vertical * v - origin,
+            );
+            let col = ray_color(r);
+            d.draw_pixel(
+                i,
+                j,
+                Color::new(
+                    (col.x * 255.999) as u8,
+                    (col.y * 255.999) as u8,
+                    (col.z * 255.999) as u8,
+                    255,
+                ),
+            )
+        }
+    }
+
+    drop(d);
+
+    while !rl.window_should_close() {
+        let _d = rl.begin_drawing(&thread);
+    }
 }
---
+++ src/vec3.rs
@@ -0,0 +1,111 @@
+use std::ops;
+
+#[derive(Clone, Copy, Debug)]
+pub struct Vec3 {
+    pub y: f64,
+    pub x: f64,
+    pub z: f64,
+}
+
+impl ops::Add for Vec3 {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self {
+        Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }
+    }
+}
+
+impl ops::Sub for Vec3 {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self {
+        Self {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
+        }
+    }
+}
+
+impl ops::Mul for Vec3 {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
+        }
+    }
+}
+
+impl ops::Div for Vec3 {
+    type Output = Self;
+
+    fn div(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x / rhs.x,
+            y: self.y / rhs.y,
+            z: self.z / rhs.z,
+        }
+    }
+}
+
+impl ops::Mul<f64> for Vec3 {
+    type Output = Self;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        Self {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs,
+        }
+    }
+}
+
+impl ops::Div<f64> for Vec3 {
+    type Output = Self;
+
+    fn div(self, rhs: f64) -> Self::Output {
+        Self {
+            x: self.x / rhs,
+            y: self.y / rhs,
+            z: self.z / rhs,
+        }
+    }
+}
+
+impl Vec3 {
+    pub const fn new(x: f64, y: f64, z: f64) -> Vec3 {
+        Vec3 { x, y, z }
+    }
+
+    pub fn dot(&mut self, v: Vec3) -> f64 {
+        self.x * v.x + self.y * v.y + self.z + v.z
+    }
+
+    pub fn cross(&mut self, v: Vec3) -> Vec3 {
+        Vec3 {
+            x: self.y * v.z - self.z * v.y,
+            y: self.z * v.x - self.x * v.z,
+            z: self.x * v.y - self.y * v.x,
+        }
+    }
+
+    pub fn normalize(mut v: Vec3) -> Vec3 {
+        let len = v.lenght();
+        v / len
+    }
+
+    pub fn lenght_squared(&mut self) -> f64 {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+
+    pub fn lenght(&mut self) -> f64 {
+        self.lenght_squared().sqrt()
+    }
+}
